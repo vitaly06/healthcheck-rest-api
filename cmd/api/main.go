9e78e1dd7da7a6ab8161631848e86105a0ab1481
unknown <vitaly.sadikov1@yandex.ru>
2026-06-07 19:02:22 +0500

    feat: add endpoints

---
+++ cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "github.com/gofiber/fiber/v3"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/health", func(c fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status": "ok",
+		})
+	})
+
+	app.Get("/ping", func(c fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "pong",
+		})
+	})
+
+	app.Get("/version", func(c fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"name":    "healthcheck-rest-api",
+			"version": "1.0.0",
+			"status":  "ok",
+		})
+	})
+
+	app.Listen(":3000")
+}
